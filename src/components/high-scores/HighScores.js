1ffdee1dea79da567592e7a6a255adc035a3b502
George Jone <g.george.jone@gmail.com>
2019-11-21 14:41:49 -0800

    separate loading setstate from high scores data setstate

--- src/components/high-scores/HighScores.js
+++ src/components/high-scores/HighScores.js
@@ -19,7 +19,10 @@ class HighScores extends React.Component {
     fetch(url)
      .then( data => data.json() )
      .then( data => {
-      this.setState({highScores: data, isLoading: false})
+      this.setState({highScores: data})
+     })
+     .then( () => {
+       this.setState({isLoading: false});
      })
      .catch( err => console.log(err) );
   }
